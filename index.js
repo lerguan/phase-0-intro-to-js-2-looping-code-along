ea0e5b6a72c530d83d5c2792bccf839b5b534d35
Leroy G <liguan698@gmail.com>
2023-03-04 23:54:55 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const message = [];
+function writeCards(nameArray, eventName) {
+    for (let i=0; i < nameArray.length; i++) {
+        message.push(`Thank you, ${nameArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return message;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number --;
+    }
+}
